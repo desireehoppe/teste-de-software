ad8d4350019206a6fde238de6d426ff743ccb8f2
desireehoppe <desihoppe@gmail.com>
2017-04-01 13:46:01 -0300

    Finalização do exercício 03 - 01/04/2017

--- src/exercício3/TesteIntervalo.java
+++ src/exercício3/TesteIntervalo.java
@@ -12,6 +12,7 @@ public class TesteIntervalo {
 	
 	@Test
 	public void testeIntervaloDias() throws Exception {
+		
 		DateTime dataInicio = new DateTime(2017,3,31, 18, 30);
 		DateTime dataFim = new DateTime(2017,5,6, 18, 30);
 		DateTime hoje = new DateTime(2017, 4, 1, 18, 30);
@@ -19,12 +20,9 @@ public class TesteIntervalo {
 		Interval intervalo = new Interval(dataInicio, dataFim);
 		assertTrue(intervalo.contains(hoje));
 	
-		long periodo = intervalo.toDurationMillis();
-		
-		long dias = periodo / 86400000            ;
-		
-		System.out.print(dias);
-		
+		long periodo = intervalo.toDurationMillis();		
+		long dias = periodo / 86400000;
+		assertEquals(36, dias);
 	}
 
 }
